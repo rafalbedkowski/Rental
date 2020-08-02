c46614d8229cbc9bf6c83e5da78293d61ba70244
Rafal Bedkowski <rafal.bedkowski@gmail.com>
2020-08-02 22:00:22 +0200

    create database

--- Rental.BLL/DalContext/RentalContext.cs
+++ Rental.BLL/DalContext/RentalContext.cs
@@ -1,12 +1,15 @@
+using System;
 using Rental.DAL.Models;
 using System.Data.Entity;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Rental.BLL.IDalContext;
+using Rental.BLL.Migrations;
 
 
 namespace Rental.Bll.DalContext
 {
-    public class RentalContext : DbContext, IRentalContext
+    public class RentalContext : DbContext, IRentalContext, IDisposable
     {
         private readonly IRentalContext _db;
 
@@ -14,22 +17,25 @@ namespace Rental.Bll.DalContext
         {
             _db = db;
         }
+
         public RentalContext()
             : base(nameOrConnectionString: "RentalContext")
         {
-            Database.SetInitializer<RentalContext>(null);
+            try
+            {
+                this.Database.Connection.Open();
+                this.Database.Connection.Close();
+            }
+            catch
+            {
+                Database.SetInitializer<RentalContext>(new DropCreateDatabaseAlways<RentalContext>());
+            }
         }
-
-        public static RentalContext Create()
-        {
-            return new RentalContext();
-        }
-
-        public void SaveChanges() => base.SaveChanges();
 
         public DbSet<User> Users { get; set; }
         public DbSet<Company> Company { get; set; }
         public DbSet<Tool> Tools { get; set; }
         public DbSet<Transaction> Transactions { get; set; }
+        public void SaveChanges() => base.SaveChanges();
     }
 }
